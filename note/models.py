20c84ccb30f6eae16eb112ad7954d897e8b0e85d
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-05-07 22:04:00 +0300

    📝 Create Model for note

--- note/models.py
+++ note/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="subjects"
+    )
+
+
+class Lesson(models.Model):
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name="lessons"
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
